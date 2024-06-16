d3c378d374fd5993b5c180554309b0907aa1c802
Francesco <francesco.loiacono@linksmt.it>
2024-06-16 20:23:37 +0200

    testing composable

--- app.vue
+++ app.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-const state = useSharedState()
+const state = useSharedState<{count: number}>()
+state.value.count ||= 0
 </script>
 
 <template>
   <div>
     {{ state }}
+    <button @click="state.count++">Increment</button>
   </div>
 </template>
