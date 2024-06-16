8da6f678bfaa576122217ccd334b2e812936f4ae
Francesco <francesco.loiacono@linksmt.it>
2024-06-16 20:17:39 +0200

    update type and forced response type

--- composables/shared-state.ts
+++ composables/shared-state.ts
@@ -1,10 +1,12 @@
-export function useSharedState(id: string = 'state') {
-    const state = useState(() => ({}))
+export function useSharedState<T extends Record<string, any>>(id: string = 'state') {
+    const state = useState(() => ({} as T))
 
     if (import.meta.server) {
         onServerPrefetch(async () => {
-            const data = await $fetch(`/api/state/${id}`)
-            state.value = data
+            const data = await $fetch(`/api/state/${id}`, {
+                responseType: 'json'
+            })
+            state.value = data as T || {}
         })
     }
 
