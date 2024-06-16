5d5aba8899e689500c7860ab9998b0c5dae2c899
Francesco <francesco.loiacono@linksmt.it>
2024-06-16 20:15:04 +0200

    set state value on server prefetch

--- composables/shared-state.ts
+++ composables/shared-state.ts
@@ -1,3 +1,12 @@
-export function useSharedState() {
-    return useState(() => ({}))
+export function useSharedState(id: string = 'state') {
+    const state = useState(() => ({}))
+
+    if (import.meta.server) {
+        onServerPrefetch(async () => {
+            const data = await $fetch(`/api/state/${id}`)
+            state.value = data
+        })
+    }
+
+    return state
 }
